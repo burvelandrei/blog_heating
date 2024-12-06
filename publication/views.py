1655e541d0ac26c0119e6fc02a41ece28cfa62bf
Andrei Burvel <burvelandrei@gmail.com>
2024-12-06 14:30:05 +0300

    V 1.6 добавлена фильтрация по категориям

--- publication/views.py
+++ publication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Publication, Article
+from category.models import Category
 
 
 class PublicationListView(ListView):
@@ -9,10 +10,21 @@ class PublicationListView(ListView):
     context_object_name = 'publications'
     paginate_by = 10
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(article__category_id=category_id) | queryset.filter(video__category_id=category_id)
+        return queryset.order_by(self.get_ordering())
+
     def get_ordering(self):
-        ordering = self.request.GET.get('sort', 'title')
+        ordering = self.request.GET.get('sort', 'title')  
         return ordering
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class PublicationDetailView(DetailView):
     model = Publication
