761a91a9054eb562fb0e3b540f3d7ea1bc017769
Andrei Burvel <burvelandrei@gmail.com>
2024-12-07 13:42:57 +0300

    V 1.11 доьавил описание в категории и теги

--- tag/models.py
+++ tag/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=20, null=False, verbose_name="Наименование")
+    description = models.TextField(blank=True, verbose_name="Описание")
 
     def __str__(self):
         return f"{self.name}"
